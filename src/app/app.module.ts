ae2587269ff545becf4382e5d68cc38626ec20f5
BrunoAssengo <brunoassengo@me.com>
2023-09-18 19:58:34 -0300

    Remoção de componentes anteriores criados p/ teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Component1Component } from './component1/component1.component';
-import { Component2Component } from './component2/component2.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BATATALComponent } from './batatal/batatal.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
@@ -26,9 +23,6 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     AppComponent,
-    Component1Component,
-    Component2Component,
-    BATATALComponent,
     InicioComponent,
     LoginComponent,
     MenuComponent,
