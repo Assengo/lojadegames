6d7b84e1da50498f94c8ec0c12fb8af8d49a5239
BrunoAssengo <brunoassengo@me.com>
2023-09-18 19:51:44 -0300

    Projeto atividade 1 SENAI

---
+++ src/app/batatal/batatal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-batatal',
+  templateUrl: './batatal.component.html',
+  styleUrls: ['./batatal.component.css']
+})
+export class BATATALComponent {
+
+}
